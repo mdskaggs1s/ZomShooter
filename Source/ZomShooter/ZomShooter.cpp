70804b3f8719553a07b9f5240e8bdcda2e183dd2
mdskaggs1s <matthewskaggs36@yahoo.com>
2018-05-16 19:00:40 -0500

    adding player movement

---
+++ Source/ZomShooter/ZomShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ZomShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZomShooter, "ZomShooter" );
